ac8e26b2a7f27b42f92e2c366e939dac3fdd6e08
RawalAnuma <rawalanuma@gmail.com>
2025-07-16 13:02:23 +0545

    mvc concept is implemented

---
+++ src/ElectricityBilling.java
@@ -0,0 +1,8 @@
+import view.BillingView;
+
+public class ElectricityBilling {
+    public static void main(String[] args) {
+        BillingView billingView = new BillingView();
+        billingView.showBillingView();
+    }
+}
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,26 +1,44 @@
 package model;
 
 public class Customer {
-    private String name;
-    private String houseNumber;
+    private String customerName;
+    private int houseNumber;
     private double unitsConsumed;
+    private double billAmount;
 
-    public Customer(String name, String houseNumber, double unitsConsumed){
-        this.name = name;
+    public Customer(String customerName, int houseNumber, double unitsConsumed){
+        this.customerName = customerName;
         this.houseNumber = houseNumber;
         this.unitsConsumed = unitsConsumed;
+        this.billAmount = calculateBill();
     }
 
-    public String getName(){
-        return name;
+    public double calculateBill(){
+        double bill = 0;
+        double remainingUnits = 0;
+        if(this.unitsConsumed <= 10){
+            bill = 10 * this.unitsConsumed;
+        }else if(this.unitsConsumed <= 30){
+            remainingUnits = this.unitsConsumed - 10;
+            bill = (remainingUnits * (250.0/20.0)) + 100;
+        }else{
+            remainingUnits = this.unitsConsumed - 20 - 10;
+            bill = (remainingUnits * 2) + 100 + 250;
+        }
+        return bill;
     }
-    public void setName(String name){
-        this.name = name;
+
+
+    public String getCustomerName(){
+        return customerName;
     }
-    public String getHouseNumber(){
+    public void setCustomerName(String customerName){
+        this.customerName = customerName;
+    }
+    public int getHouseNumber(){
         return houseNumber;
     }
-    public void setHouseNumber(String houseNumber){
+    public void setHouseNumber(int houseNumber){
         this.houseNumber = houseNumber;
     }
     public double getUnitsConsumed(){
@@ -28,5 +46,11 @@ public class Customer {
     }
     public void setUnitsConsumed(double unitsConsumed){
         this.unitsConsumed = unitsConsumed;
+    }
+    public double getBillAmount() {
+        return billAmount;
+    }
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
     }
 }
