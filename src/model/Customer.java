7103918aa4edcf0c854fb5b6d08e43c9d9b44432
RawalAnuma <rawalanuma@gmail.com>
2025-07-18 10:11:46 +0545

    .\Customer model created with proper constructor and getter methods

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,31 +1,16 @@
 package model;
 
 public class Customer {
+    private int customerId;
     private String customerName;
     private int houseNumber;
     private double unitsConsumed;
-    private double billAmount;
 
-    public Customer(String customerName, int houseNumber, double unitsConsumed){
+    public Customer(int customerId, String customerName, int houseNumber, double unitsConsumed){
+        this.customerId = customerId;
         this.customerName = customerName;
         this.houseNumber = houseNumber;
         this.unitsConsumed = unitsConsumed;
-        this.billAmount = calculateBill();
-    }
-
-    public double calculateBill(){
-        double bill = 0;
-        double remainingUnits = 0;
-        if(this.unitsConsumed <= 10){
-            bill = 10 * this.unitsConsumed;
-        }else if(this.unitsConsumed <= 30){
-            remainingUnits = this.unitsConsumed - 10;
-            bill = (remainingUnits * (250.0/20.0)) + 100;
-        }else{
-            remainingUnits = this.unitsConsumed - 20 - 10;
-            bill = (remainingUnits * 2) + 100 + 250;
-        }
-        return bill;
     }
 
 
@@ -47,10 +32,5 @@ public class Customer {
     public void setUnitsConsumed(double unitsConsumed){
         this.unitsConsumed = unitsConsumed;
     }
-    public double getBillAmount() {
-        return billAmount;
-    }
-    public void setBillAmount(double billAmount) {
-        this.billAmount = billAmount;
-    }
+
 }
