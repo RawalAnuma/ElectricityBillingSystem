131e7f05839e3e27f4d9b2b3f7429455fc33fc6e
RawalAnuma <rawalanuma@gmail.com>
2025-07-18 10:25:22 +0545

    created customer controller and bill records controller ehich manages customer-related and billing operations

--- src/controller/BillRecordsController.java
+++ src/controller/BillRecordsController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import calculation.BillCalculation;
+import dao.BillRecordsDAO;
+import model.BillRecords;
+import model.Customer;
+
+import java.util.List;
+
 public class BillRecordsController {
+    private BillRecordsDAO billRecordsDAO = new BillRecordsDAO();
+    private BillCalculation billCalculation = new BillCalculation();
+
+    public double generateBill(int customerId, double unitsConsumed) {
+        double bill = billCalculation.calculateBill(unitsConsumed);
+        billRecordsDAO.insertBillRecord(customerId, bill);
+        return bill;
+    }
+
+    public List<BillRecords> getAllBillRecords() {
+        return billRecordsDAO.getAllBillRecords();
+    }
+
+    public BillRecords getHighestPayingCustomer(){
+        return billRecordsDAO.getHighestPayingCustomer();
+    }
 }
--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -8,25 +8,11 @@ import java.util.List;
 public class CustomerController {
     private CustomerDAO customerDAO = new CustomerDAO();
 
-    public void addCustomer(String customerName, int houseNumber, double unitsConsumed){
-        Customer customer = new Customer(customerName, houseNumber, unitsConsumed);
-        customerDAO.insertCustomer(customer);
+    public int addCustomer(String customerName, int houseNumber, double unitsConsumed){
+        Customer customer = new Customer(0, customerName, houseNumber, unitsConsumed);
+        return customerDAO.insertCustomer(customer);
     }
     public List<Customer> getAllCustomers(){
         return customerDAO.getAllCustomers();
-    }
-
-    public Customer getHighestBillCustomer(List<Customer> customers) {
-        Customer highestBillCustomer = null;
-        double maxBill = 0;
-
-        for (Customer customer : customers) {
-            double bill = customer.calculateBill();
-            if (bill > maxBill) {
-                maxBill = bill;
-                highestBillCustomer = customer;
-            }
-        }
-        return highestBillCustomer;
     }
 }
