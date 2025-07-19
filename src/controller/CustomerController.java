6dbd1e3efd982f60097e4442c4a46dd4ca661cb9
RawalAnuma <rawalanuma@gmail.com>
2025-07-19 22:33:17 +0545

    handle customer operations between view and DAO

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -8,11 +8,14 @@ import java.util.List;
 public class CustomerController {
     private CustomerDAO customerDAO = new CustomerDAO();
 
-    public int addCustomer(String customerName, int houseNumber, double unitsConsumed){
-        Customer customer = new Customer(0, customerName, houseNumber, unitsConsumed);
+    public boolean addCustomer(String customerName, int houseNumber, double unitsConsumed){
+        Customer customer = new Customer(customerName, houseNumber, unitsConsumed);
         return customerDAO.insertCustomer(customer);
     }
     public List<Customer> getAllCustomers(){
         return customerDAO.getAllCustomers();
+    }
+    public int getCustomerId(String customerName, int houseNumber, double unitsConsumed) {
+        return customerDAO.getCustomerIdByDetails(customerName, houseNumber, unitsConsumed);
     }
 }
