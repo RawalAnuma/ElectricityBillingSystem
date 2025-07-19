366397305a64d0504da99e918023b7e9e30c1819
RawalAnuma <rawalanuma@gmail.com>
2025-07-19 22:37:31 +0545

    display all customers' bill sorted by highest bill amount

--- src/view/AllCustomersBillView.java
+++ src/view/AllCustomersBillView.java
@@ -1,4 +1,35 @@
 package view;
 
+import controller.BillRecordsController;
+import dao.BillRecordsDAO;
+import model.BillRecords;
+
+import java.util.List;
+
 public class AllCustomersBillView {
+    private BillRecordsController billRecordsController = new BillRecordsController();
+    public void showAllBills(){
+        try{
+            List<BillRecords> billRecordsList = billRecordsController.getAllBillRecords();
+            if (billRecordsList.isEmpty()) {
+                System.out.println("No bill records found.");
+            } else {
+                System.out.println("All Customers Bill Records:");
+                System.out.println("-------------------------------------------------------------------------------------------------");
+                System.out.println("Bill ID    | Customer Name   | House Number  | Units Consumed  | Bill Amount    | Bill Date");
+                System.out.println("-------------------------------------------------------------------------------------------------");
+                for (BillRecords bill : billRecordsList) {
+                    System.out.printf("%-10d | %-15s | %-13d | %-15.2f | %-14.2f | %s%n",
+                            bill.getBillId(),
+                            bill.getCustomerName(),
+                            bill.getHouseNumber(),
+                            bill.getUnitsConsumed(),
+                            bill.getBillAmount(),
+                            bill.getBillDate());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
