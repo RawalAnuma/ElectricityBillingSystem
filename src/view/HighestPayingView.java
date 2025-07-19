aa340994ad462b5a8dac99fb68a203cfc525d8cb
RawalAnuma <rawalanuma@gmail.com>
2025-07-19 22:35:54 +0545

    display details of the customer with highest bill amount

--- src/view/HighestPayingView.java
+++ src/view/HighestPayingView.java
@@ -1,4 +1,25 @@
 package view;
 
+import controller.BillRecordsController;
+import model.BillRecords;
+
 public class HighestPayingView {
+    private BillRecordsController billRecordsController = new BillRecordsController();
+    public void handleHighestBill() {
+        try{
+            BillRecords highestBill = billRecordsController.getHighestPayingCustomer();
+            if (highestBill != null) {
+                System.out.println("Highest Paying Customer:");
+                System.out.println("Customer Name   : " + highestBill.getCustomerName());
+                System.out.println("House Number    : " + highestBill.getHouseNumber());
+                System.out.println("Bill Amount     : " + highestBill.getBillAmount());
+                System.out.println("Units Consumed  : " + highestBill.getUnitsConsumed());
+                System.out.println("Bill Date       : " + highestBill.getBillDate());
+            } else {
+                System.out.println("No bill records found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Error retrieving highest paying customer: " + e.getMessage());
+        }
+    }
 }
